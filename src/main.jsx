859ce90ee69577eaf355cc33f95f3171041b684f
vdhoan <156833044+vdhoan@users.noreply.github.com>
2025-03-20 01:10:04 +0700

    .

--- src/main.jsx
+++ src/main.jsx
@@ -2,9 +2,13 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.jsx'
+import LoginBar from './assets/login-bar'
+import HomeNavBar from './assets/home-navbar'
 
 createRoot(document.getElementById('root')).render(
   <StrictMode>
+    <HomeNavBar />
+    <LoginBar />
     <App />
   </StrictMode>,
 )
